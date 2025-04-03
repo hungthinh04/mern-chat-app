e956564c1da6caaeba6a1eac995f93b8a6933af6
hungthinh04 <hungthinh2004pt@gmail.com>
2025-04-04 02:39:23 +0700

    update avater  profile

--- frontend/src/page/ProfilePage.jsx
+++ frontend/src/page/ProfilePage.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuthStore } from "./../store/useAuthStore.js";
 import { Camera, User, Mail } from "lucide-react";
 
 const ProfilePage = () => {
   const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
-
-  const handleImageUpLoad = async (e) => {};
+const [selectedImg , setSelectedImg]=useState(null);
+  const handleImageUpLoad = async (e) => {
+    const file = e.target.files[0];
+    if(!file) return;
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = async()=>{
+      const base64Image = reader.result;
+      setSelectedImg(base64Image)
+      await updateProfile({profilePic:base64Image})
+    }
+  };
   return (
     <div>
       <div className="h-screen pt-20">
@@ -19,7 +29,7 @@ const ProfilePage = () => {
             <div className="flex flex-col items-center gap-4">
               <div className="relative">
                 <img
-                  src={authUser.profilePic || "/avatar.png"}
+                  src={selectedImg||authUser.profilePic || "/avatar.png"}
                   alt="Profile"
                   className="size-32 rounded-full object-cover border-4"
                 />
@@ -45,43 +55,43 @@ const ProfilePage = () => {
                   ? "Uploading..."
                   : "Click the camera icon to update your photo"}
               </p>
-
-              <div className="space-y-6">
-                <div className="space-y-1.5">
-                  <div className="text-sm text-zinc-400 flex items-center gap-2">
-                    <User className="size-4" />
-                    Full Name
-                  </div>
-                  <p className="px-4 py-2.5 bg-base-200 rounded-lg border">
-                    {authUser?.fullName}
-                  </p>
+            </div>
+            <div className="space-y-6">
+              <div className="space-y-1.5">
+                <div className="text-sm text-zinc-400 flex items-center gap-2">
+                  <User className="size-4" />
+                  Full Name
                 </div>
-
-                <div className="space-y-1.5">
-                  <div className="text-sm text-zinc-400 flex items-center gap-2">
-                    <Mail className="size-4" />
-                    Email Address
-                  </div>
-                  <p className="px-4 py-2.5 bg-base-200 rounded-lg border">
-                    {authUser?.email}
-                  </p>
-                </div>
+                <p className="px-4 py-2.5 bg-base-200 rounded-lg border">
+                  {authUser?.fullName}
+                </p>
               </div>
 
-              <div className="mt-6 bg-base-300 rounded-xl p-6">
-                <h2 className="text-lg font-medium mb-4">
-                  Account Information
-                </h2>
-                <div className="space-y-3 text-sm">
-                  <div className="flex items-center justify-between py-2 border-b border-zinc-700">
-                    <span>Member Since</span>
-                    <span>{authUser.createAt? authUser.createAt.split("T")[0]:"N/A"}</span>
-                  </div>
+              <div className="space-y-1.5">
+                <div className="text-sm text-zinc-400 flex items-center gap-2">
+                  <Mail className="size-4" />
+                  Email Address
+                </div>
+                <p className="px-4 py-2.5 bg-base-200 rounded-lg border">
+                  {authUser?.email}
+                </p>
+              </div>
+            </div>
+            <div className="mt-6 bg-base-300 rounded-xl p-6">
+              <h2 className="text-lg font-medium mb-4">Account Information</h2>
+              <div className="space-y-3 text-sm">
+                <div className="flex items-center justify-between py-2 border-b border-zinc-700">
+                  <span>Member Since</span>
+                  <span>
+                    {authUser.createAt
+                      ? authUser.createAt.split("T")[0]
+                      : "N/A"}
+                  </span>
+                </div>
 
-                  <div className="flex items-center justify-between py-2">
-                    <span>Account Status</span>
-                    <span className="text-green-500">Active</span>
-                  </div>
+                <div className="flex items-center justify-between py-2">
+                  <span>Account Status</span>
+                  <span className="text-green-500">Active</span>
                 </div>
               </div>
             </div>
